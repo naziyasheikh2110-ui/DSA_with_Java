4eedd816a6c10dcb6cfa9059f695d98775b90c60
naziyasheikh2110 <naziyasheikh2110@gmail.com>
2026-04-06 19:47:05 +0530

    Initial commit

---
+++ VariableAndDatatypes.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class VariableAndDatatypes {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+//       String input = sc.next();
+//       System.out.println(input);
+
+//        String name = sc.nextLine();
+//        System.out.println(name);
+//
+//        int number = sc.nextInt();
+//        System.out.println(number);
+
+
+//   VARIABLE -> a container that stores value
+//        int a = 20;
+//        int b = 15;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//   DATATYPES
+//        byte b = 3;
+//        char ch = 'a';
+//        boolean var = false;
+//        double price = 30.5;  // long is similar to double
+//        float fl = 32.0f;
+//        int num = 43;
+//        short n = 240;
+
+//   PRACTICE
+//        int a = 4;
+//        int b = 7;
+//        int sum = a + b;
+//        System.out.println(sum);
+
+//      ->Sum and product of two numbers
+//        System.out.println("Enter your first number: ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter your second number");
+//        int b = sc.nextInt();
+//
+//        int sum = a + b ;
+//        int product = a * b ;
+//        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+//        System.out.println("The product of " + a + " and " + b + " is " + product);
+
+//      -> Area of circle
+//        System.out.println("Enter the area of Circle: ");
+//        float r = sc.nextFloat();
+//        float area = 3.14f * r * r ;
+//        System.out.println("Area of circle is -> " + area);
+
+//      -> Avg me 3 numbers
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//
+//        int avg = (a+b+c)/3;
+//        System.out.println(avg);
+
+//      -> Area of Square
+//         int side = sc.nextInt();
+//         int area = side * side ;
+//        System.out.println(area);
+
+//      -> Adding tax
+//        float pen = sc.nextInt();
+//        float pencil = sc.nextInt();
+//        float eraser = sc.nextInt();
+//        float totalCost = pen + pencil + eraser ;
+//        float addGST = totalCost + (totalCost * 18 / 100);
+//
+//        System.out.println("Cost of pen = " + pen + "\nCost of Pencil = " + pencil + "\nCost of eraser = " + eraser );
+//        System.out.println("Total Cost = " + totalCost);
+//        System.out.println("final amount after 18% GST = " + addGST);
+
+//      -> Type Casting
+//           float a = 21.4f;
+//           int b = (int)a;
+
+//      -> Type Promotion
+//          char a = 'a';
+//          char b = 'b';
+//        System.out.println(b-a);
+
+
+    }
+}

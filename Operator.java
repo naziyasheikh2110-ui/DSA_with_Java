87336f3640d28090462aa2973058582402af4631
naziyasheikh2110 <naziyasheikh2110@gmail.com>
2026-04-07 17:09:35 +0530

    Initial commit

---
+++ Operator.java
@@ -0,0 +1,99 @@
+public class Operator {
+    public static void main(String[] args) {
+
+//  1. BINARY OPERATOR
+//      int A = 10;
+//      int B = 5;
+//
+//      System.out.println("Add = " + (A+B));
+//      System.out.println("Subtract = " + (A-B));
+//      System.out.println("Multiply = " + (A*B));
+//      System.out.println("Divide = " + (A/B));
+//      System.out.println("Modulo = " + (A%B));
+
+//  2. UNARY OPERATOR
+//     -> pre-increment
+//        int a = 10;
+//        int b = ++a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+ //    -> post-increment
+//        int a = 10;
+//        int b = a++;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//      -> pre-decrement
+//        int a = 10;
+//        int b = --a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//      -> post-decrement
+//         int a = 10;
+//         int b = a--;
+//         System.out.println(a);
+//         System.out.println(b);
+
+//  3. RELATIONAL OPERATOR
+//       int a = 10;
+//       int b = 5;
+//       System.out.println(a==b);
+//       System.out.println(a!=b);
+//       System.out.println(a<b);
+//       System.out.println(a>b);
+//       System.out.println(a<=b);
+//       System.out.println(a>=b);
+
+//  4. LOGICAL OPERATOR
+//     -> logical AND (if both cases are true then final ans is true otherwise it will be false)
+//        System.out.println( (3>1) && (4>0));
+//        System.out.println( (3<0) && (4>0));
+
+//     -> logical OR (if either of case is true then final ans will be true)
+//        System.out.println( (3>1) || (4>0));
+//        System.out.println( (3<0) || (4>0));
+//        System.out.println( (3<1) || (4<0));
+
+//     -> logical NOT (it will give opposite of your answer)
+//        System.out.println( !(3>1) );
+
+//  5. ASSIGNMENT OPERATOR
+//        int a = 10;
+//
+//        System.out.println(a+=10); //(a=a+10)
+//        System.out.println(a-=5);
+//        System.out.println(a*=2);
+//        System.out.println(a/=2);
+//        System.out.println(a%=2);
+
+
+
+//      PRACTICE QUESTIONS
+//       int x = 2, y = 5;
+//
+//       int exp1 = (x * y / x);
+//       int exp2 = (x * (y / x));
+//       System.out.print(exp1 + ",") ;
+//       System.out.print(exp2);
+
+//        int x = 200, y = 50, z = 100;
+//        if(x > y && y > z){
+//            System.out.println("Hello");
+//        }
+//        if(z > y && z < x){
+//            System.out.println("Java");
+//        }
+//        if((y+200) < x && (y+150) < z){
+//            System.out.println("Hello Java");
+//        }
+
+//        int x, y, z;x = y = z = 2;
+//        x += y;
+//        y -= z;
+//        z /= (x + y);
+//        System.out.println(x + " " + y + " " + z);
+
+    }
+}
